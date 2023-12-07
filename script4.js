be6c47d65fbd9cb9c286a5c141712ff44ab58002
nfzchallengelist <153219930+nfzchallengelist@users.noreply.github.com>
2023-12-07 19:26:30 +0200

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,108 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var levels = [
+    //top row is #1 on the list
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' },
+    { name: 'LevelName', id: 'LevelID', author: 'LevelCreator', verifier: 'LevelVerifier', videoUrl: 'YOUTUBEURLRIGHTHERE', type: 'Challenge' }
+    //don't put a comma at the end of the last level..
+  ];
+
+
+  var legacyList = document.getElementById('legacyList');
+
+  levels.forEach(function(level, index) {
+    var listItem = document.createElement('div');
+    listItem.classList.add('list-item');
+    var listNumber = document.createElement('div');
+    listNumber.classList.add('list-number');
+    listNumber.textContent = '#' + (index + 1);
+    var listThumbnail = document.createElement('div');
+    listThumbnail.classList.add('list-thumbnail');
+    listThumbnail.style.pointerEvents = 'none'; // Add this line
+    var thumbnailLink = document.createElement('a');
+    thumbnailLink.href = level.videoUrl;
+    thumbnailLink.target = '_blank';
+    var thumbnailImage = document.createElement('img');
+    thumbnailImage.classList.add('list-image');
+    thumbnailImage.alt = 'Level thumbnail';
+    var videoId = extractVideoId(level.videoUrl);
+    var thumbnailUrl = 'https://img.youtube.com/vi/' + videoId + '/mqdefault.jpg';
+    thumbnailImage.src = thumbnailUrl;
+    thumbnailLink.appendChild(thumbnailImage);
+    listThumbnail.appendChild(thumbnailLink);
+    var listInfo = document.createElement('div');
+    listInfo.classList.add('list-info');
+    var levelName = document.createElement('p');
+    levelName.classList.add('level-name');
+    levelName.textContent = level.name;
+    listInfo.appendChild(levelName);
+    listItem.appendChild(listNumber);
+    listItem.appendChild(listThumbnail);
+    listItem.appendChild(listInfo);
+    legacyList.appendChild(listItem);
+
+  
+// Display the popup
+listItem.addEventListener('click', function() {
+  // Get the video ID from the video URL
+  var videoId = extractVideoId(levels[index].videoUrl);
+
+  // Create the iframe element for the embedded video
+  var iframe = document.createElement('iframe');
+  iframe.src = 'https://www.youtube.com/embed/' + videoId;
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.frameborder = '0';
+  iframe.allowfullscreen = true;
+  iframe.style.border = 'none';
+  iframe.style.borderRadius = "10px";
+
+  // Get the popup element
+  var popup = document.getElementById('popup');
+
+  // Clear the existing content of 'popup-video'
+  var popupVideo = document.getElementById('popup-video');
+  popupVideo.innerHTML = '';
+
+  // Append the iframe to the popup video
+  popupVideo.appendChild(iframe);
+
+  // Populate the popup with the corresponding information
+  document.getElementById('popup-name').textContent = levels[index].name;
+  document.getElementById('popup-id').textContent = levels[index].id;
+  document.getElementById('popup-author').textContent = levels[index].author;
+  document.getElementById('popup-verifier').textContent = levels[index].verifier;
+
+  // Display the popup
+  popup.classList.add('visible');
+});
+
+// Close the popup when the close button is clicked
+document.getElementById('popup-close').addEventListener('click', function() {
+  var popup = document.getElementById('popup');
+  popup.classList.remove('visible');
+
+  // Get the iframe element
+  var iframe = document.getElementById('popup-video').querySelector('iframe');
+
+  // Pause the video and reset the source
+  iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  iframe.src = '';
+});
+
+  function extractVideoId(url) {
+    var match = url.match(/(?:[?&]v=|\/embed\/|\/\d\/|\/vi\/|youtu\.be\/|\/embed\/|\/\d\/|\/vi\/)([^#&?/]+)/i);
+    if (match && match[1]) {
+      return match[1];
+    } else {
+      return '';
+    }
+  }
+})});
