8ab74335a833432a56a2c9583e995dd7e9bffdf8
nfzchallengelist <153219930+nfzchallengelist@users.noreply.github.com>
2023-12-17 17:28:37 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -71,10 +71,10 @@ window.addEventListener('DOMContentLoaded', function() {
     { name: '4 seconds icremrob', id: '93302229', author: 'Icecreamrib', verifier: 'Icecreamrib', videoUrl: 'https://youtu.be/mXbGVM0FXrk', type: 'Challenge' },
     
     
-    { name: 'nfzcls', id: '92036087', author: 'Martijn07', verifier: 'Martijn07', videoUrl: 'https://youtu.be/ScPaEm-5HME', type: 'Challenge' },
-
-
     { name: 'Leaves', id: '97306244', author: 'xFinleyGD', verifier: 'xFinleyGD', videoUrl: 'videourl', type: 'Challenge' },
+
+    
+    { name: 'nfzcls', id: '92036087', author: 'Martijn07', verifier: 'Martijn07', videoUrl: 'https://youtu.be/ScPaEm-5HME', type: 'Challenge' },
 
     
     { name: 'airborne', id: '92186881', author: 'xFinleyGD', verifier: 'xFinleyGD', videoUrl: 'https://youtu.be/8bqC1GO6BIs', type: 'Challenge' },
