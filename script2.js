5a7a95a649b6b215a75c7d85a06ac90e9f9f36ee
nfzchallengelist <153219930+nfzchallengelist@users.noreply.github.com>
2024-03-03 08:36:24 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -231,7 +231,7 @@ window.addEventListener('DOMContentLoaded', function() {
     { name: 'diego aint clearing', id: '92059623', author: 'Martijn07', verifier: 'Martijn07', videoUrl: 'https://youtu.be/K_c02JMp48g', type: 'Challenge' },
 
     
-    { name: 'Get it Together', id: '92035976', author: 'Shadowpan69420', verifier: 'iamfinleyy', videoUrl: 'https://youtu.be/iXu3G6CAMX0', type: 'Challenge' },
+    { name: 'Get it Together', id: '92035976', author: 'Shadowpan69420', verifier: 'iamfinleyy', videoUrl: 'videourl', type: 'Challenge' },
     
   
     { name: 'actual', id: '92054335', author: 'Martijn07', verifier: 'Martijn07', videoUrl: 'https://youtu.be/xDwMBFhVIk0', type: 'Challenge' },
